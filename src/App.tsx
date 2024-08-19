7320632452a4424adbf93c98d0e4eeb979ce2b06
romatallinn <sirokov@dizconto.com>
2024-08-19 23:24:00 +0200

    Add github star button

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import './global.css'
 import { SystemProvider } from '@/components/providers/SystemProvider.tsx'
 import { PowerSyncContent } from '@/components/molecules/PowerSyncContent.tsx'
 import BuiltWithLogos from '@/components/molecules/BuiltWithLogos.tsx'
+import GithubStarButton from '@/components/molecules/GithubStarButton.tsx'
 
 function App() {
     return (
         <SystemProvider>
             <div className="mx-32 my-32 m-auto text-center space-y-3">
                 <BuiltWithLogos />
-                <h1 className="text-3xl">Welcome to PowerSync Tauri!</h1>
+                <h1 className="text-3xl items-center">
+                    Welcome to PowerSync Tauri!
+                </h1>
+                <GithubStarButton />
                 <PowerSyncContent />
             </div>
         </SystemProvider>
---
+++ src/components/molecules/GithubStarButton.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button.tsx'
+import { Star } from 'lucide-react'
+
+export default function GithubStarButton() {
+    return (
+        <Button size="sm" asChild>
+            <a
+                href="https://github.com/romatallin/powersync-tauri"
+                target="_blank"
+            >
+                <Star size={16} className="mr-1" /> Star Github
+            </a>
+        </Button>
+    )
+}
