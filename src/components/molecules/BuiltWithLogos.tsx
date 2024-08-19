4bc373a6dbe0ce83f3327ed0eb53b0206a0ceb1a
romatallinn <sirokov@dizconto.com>
2024-08-19 23:30:08 +0200

    Add hover effects to logos

--- src/components/molecules/BuiltWithLogos.tsx
+++ src/components/molecules/BuiltWithLogos.tsx
@@ -1,24 +1,26 @@
 import reactLogo from '@/assets/react.svg'
 
 export default function BuiltWithLogos() {
+    // Don't want to put it to global css, so it's easier to remove.
+    const logoClass = 'w-10 h-10 hover:scale-125 transition-all duration-300'
     return (
         <div className="flex flex-row space-x-3 justify-center">
             <a href="https://vitejs.dev" target="_blank">
-                <img src="/vite.svg" className="w-10 h-10" alt="Vite logo" />
+                <img src="/vite.svg" className={logoClass} alt="Vite logo" />
             </a>
             <a href="https://tauri.app" target="_blank">
-                <img src="/tauri.svg" className="w-10 h-10" alt="Tauri logo" />
+                <img src="/tauri.svg" className={logoClass} alt="Tauri logo" />
             </a>
             <a href="https://reactjs.org" target="_blank">
-                <img src={reactLogo} className="w-10 h-10" alt="React logo" />
+                <img src={reactLogo} className={logoClass} alt="React logo" />
             </a>
             <a href="https://ui.shadcn.com" target="_blank">
-                <img src="/shadcn.svg" className="w-10 h-10" alt="React logo" />
+                <img src="/shadcn.svg" className={logoClass} alt="React logo" />
             </a>
             <a href="https://powersync.com" target="_blank">
                 <img
                     src="/powersync.svg"
-                    className="w-10 h-10"
+                    className={logoClass}
                     alt="React logo"
                 />
             </a>
