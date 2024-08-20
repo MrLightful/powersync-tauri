c395a154773459627bbf46dc852d612ae34d5d28
romatallinn <sirokov@dizconto.com>
2024-08-20 02:12:45 +0200

    Add tooltips to logos

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import { SystemProvider } from '@/components/providers/SystemProvider.tsx'
 import { PowerSyncContent } from '@/components/molecules/PowerSyncContent.tsx'
 import BuiltWithLogos from '@/components/molecules/BuiltWithLogos.tsx'
 import GithubStarButton from '@/components/molecules/GithubStarButton.tsx'
+import { TooltipProvider } from './components/ui/tooltip'
 
 function App() {
     return (
         <SystemProvider>
-            <div className="mx-32 my-32 m-auto text-center space-y-3">
-                <BuiltWithLogos />
-                <h1 className="text-3xl items-center">
-                    Welcome to PowerSync Tauri!
-                </h1>
-                <GithubStarButton />
-                <PowerSyncContent />
-            </div>
+            <TooltipProvider>
+                <div className="mx-32 my-32 m-auto text-center space-y-3">
+                    <BuiltWithLogos />
+                    <h1 className="text-3xl items-center">
+                        Welcome to PowerSync Tauri!
+                    </h1>
+                    <GithubStarButton />
+                    <PowerSyncContent />
+                </div>
+            </TooltipProvider>
         </SystemProvider>
     )
 }
--- src/components/molecules/BuiltWithLogos.tsx
+++ src/components/molecules/BuiltWithLogos.tsx
@@ -1,33 +1,53 @@
 import reactLogo from '@/assets/react.svg'
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger
+} from '@/components/ui/tooltip.tsx'
 
 export default function BuiltWithLogos() {
-    // Don't want to put it to global css, so it's easier to remove.
-    const logoClass = 'w-10 h-10 hover:scale-125 transition-all duration-300'
     return (
-        <div className="flex flex-row space-x-3 justify-center">
-            <a href="https://vitejs.dev" target="_blank">
-                <img src="/vite.svg" className={logoClass} alt="Vite logo" />
-            </a>
-            <a href="https://tauri.app" target="_blank">
-                <img src="/tauri.svg" className={logoClass} alt="Tauri logo" />
-            </a>
-            <a href="https://reactjs.org" target="_blank">
-                <img src={reactLogo} className={logoClass} alt="React logo" />
-            </a>
-            <a href="https://ui.shadcn.com" target="_blank">
-                <img
-                    src="/shadcn.svg"
-                    className={logoClass}
-                    alt="Shadcn UI logo"
-                />
-            </a>
-            <a href="https://powersync.com" target="_blank">
-                <img
-                    src="/powersync.svg"
-                    className={logoClass}
-                    alt="PowerSync logo"
-                />
-            </a>
+        <div className="flex flex-row justify-center">
+            <LogoLink href="https://vitejs.dev" src="/vite.svg" alt="Vite" />
+            <LogoLink href="https://tauri.app" src="/tauri.svg" alt="Tauri" />
+            <LogoLink href="https://reactjs.org" src={reactLogo} alt="React" />
+            <LogoLink
+                href="https://ui.shadcn.com"
+                src="/shadcn.svg"
+                alt="shadcn/ui"
+            />
+            <LogoLink
+                href="https://powersync.com"
+                src="/powersync.svg"
+                alt="PowerSync"
+            />
         </div>
+    )
+}
+
+function LogoLink({
+    href,
+    src,
+    alt
+}: {
+    href: string
+    src: string
+    alt: string
+}) {
+    return (
+        <Tooltip>
+            <TooltipTrigger asChild>
+                <a href={href} target="_blank" className="mx-2">
+                    <img
+                        className="w-10 h-10 hover:scale-125 transition-all duration-300"
+                        src={src}
+                        alt={alt}
+                    />
+                </a>
+            </TooltipTrigger>
+            <TooltipContent>
+                <p>{alt}</p>
+            </TooltipContent>
+        </Tooltip>
     )
 }
