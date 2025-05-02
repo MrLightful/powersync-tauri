5b60e2693935a56b16420d8595c7b39d4192ebe5
MrLightful <romatallinn@gmail.com>
2025-05-02 16:21:39 +0200

    Add error boundary for the entire app

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,13 +1,17 @@
 import { PowerSyncProvider } from '@/hooks/powersync/powersync-provider.tsx'
 import { ReactNode, Suspense } from 'react'
 import { TooltipProvider } from '@radix-ui/react-tooltip'
+import AppErrorPage from '@/features/errors/app-error.tsx'
+import { ErrorBoundary } from 'react-error-boundary'
 
 export default function AppProvider({ children }: { children: ReactNode }) {
     return (
         <Suspense fallback={<>Loading...</>}>
-            <PowerSyncProvider>
-                <TooltipProvider>{children}</TooltipProvider>
-            </PowerSyncProvider>
+            <ErrorBoundary FallbackComponent={AppErrorPage}>
+                <PowerSyncProvider>
+                    <TooltipProvider>{children}</TooltipProvider>
+                </PowerSyncProvider>
+            </ErrorBoundary>
         </Suspense>
     )
 }
